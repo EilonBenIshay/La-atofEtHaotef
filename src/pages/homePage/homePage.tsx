a86df44aa2411dcfcf8c60c508c6a1a0f3cda39e
Efrat <118221463+EfratLopez@users.noreply.github.com>
2024-05-22 19:03:12 +0300

    feat(home-page): add home page and main buttons

---
+++ src/pages/homePage/homePage.tsx
@@ -0,0 +1,47 @@
+import GiverImage from '../../assets/giverImage.jpg';
+import RecieverImage from '../../assets/recieverImage.jpg';
+import Card from '@mui/material/Card';
+import * as s from './styles'
+
+function HomePage() {
+
+    const handleClick = () => {
+        alert("hiiiiiii")
+    };
+
+    return (
+        <>
+            <Card sx={{
+                backgroundColor: '#ECF39E',
+                height: '13rem',
+                width: '33rem',
+                position: 'absolute',
+                right: 0,
+                top: '56%',
+                transform: 'translateY(-50%)',
+                borderRadius: '150px 0 0 150px',
+                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+            }}>
+                <s.RightImage src={GiverImage} alt="Card Image" onClick={handleClick} />
+                <s.RightRotatedText>למתן סיוע</s.RightRotatedText>
+            </Card>
+
+            <Card sx={{
+                backgroundColor: '#ECF39E',
+                height: '13rem',
+                width: '33rem',
+                position: 'absolute',
+                left: 0,
+                top: '75%',
+                transform: 'translateY(-50%)',
+                borderRadius: '0 150px 150px 0',
+                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+            }}>
+                <s.LeftImage src={RecieverImage} alt="Card Image" onClick={handleClick} />
+                <s.LeftRotatedText>לקבלת סיוע</s.LeftRotatedText>
+            </Card>
+        </>
+    )
+}
+
+export default HomePage;
