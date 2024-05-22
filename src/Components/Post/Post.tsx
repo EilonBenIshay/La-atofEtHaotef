07da1fcbb186745fbe9e2ac2e8b5cfc7bd5468f5
Omerb90 <omerb90@gmail.com>
2024-05-22 23:15:49 +0300

    feat(Posts): add the post Component

---
+++ src/Components/Post/Post.tsx
@@ -0,0 +1,47 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
+import IconButton from '@mui/material/IconButton';
+import { PostAction } from '../../types/Post/Post';
+
+interface PostProps {
+    img: string;
+    description: string;
+    actions: PostAction[];
+}
+
+const Post: React.FC<PostProps> = ({ img, description, actions }) => {
+    return (
+        <Card sx={{ marginTop: 5, maxWidth: 400, backgroundColor: '#4F772D' }}>
+            <CardContent>
+                <CardMedia
+                    component="img"
+                    height="194"
+                    image={img}
+                />
+                <Card sx={{ backgroundColor: '#ECF39E', marginTop: '1rem', borderRadius: '1.25rem' }}>
+                    <CardContent>
+                        <Typography>
+                            {description}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </CardContent>
+            <CardActions sx={{ justifyContent: 'center', gap: 2 }}>
+                {actions.map((action, index) => (
+                    <IconButton key={index} onClick={action.onClickAction} sx={{
+                        borderRadius: '50%',
+                        padding: '10px',
+                        backgroundColor: '#ECF39E',
+                    }}>
+                        {action.IconImage}
+                    </IconButton>
+                ))}
+            </CardActions>
+        </Card>
+    );
+};
+
+export default Post;
--- src/pages/postPage/postPage.tsx
+++ src/pages/postPage/postPage.tsx
@@ -1,6 +1,31 @@
+import { Container } from "@mui/material";
+import Post from "../../Components/Post/Post";
+import './postPage.css'
+
 function PostPage() {
     return (
-        <>hiiii</>
+        <>
+            <Container sx={{ 
+                display: 'flex', 
+                alignItems: 'center', 
+                justifyContent: 'center', 
+            }}>
+                <Post
+                    img="https://img.freepik.com/free-photo/researchers-looking-alternative-energy-souces_23-2149311493.jpg"
+                    description="Description"
+                    actions={[
+                        {
+                            IconImage: <img src="https://img.icons8.com/color/48/000000/like--v1.png" alt="like" />,
+                            onClickAction: () => console.log('like')
+                        },
+                        {
+                            IconImage: <img src="https://img.icons8.com/color/48/000000/share--v1.png" alt="share" />,
+                            onClickAction: () => console.log('share')
+                        }
+                    ]}
+                />
+            </Container>
+        </>
     )
 }
 
