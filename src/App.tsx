ef18b0900295990456b08fddeca145477f7a6a95
Efrat <118221463+EfratLopez@users.noreply.github.com>
2024-05-22 20:43:37 +0300

    fix(home-screen): footer and fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import "./App.css";
 import HomePage from "./pages/homePage/homePage";
 import Header from "./Components/Header/Header"
+import { Divider } from "@mui/material";
 
 function App() {
   return (
     <>
       <Header></Header>
-<HomePage></HomePage>;
+      <HomePage></HomePage>;
+
+      <Divider variant="middle"
+        sx={{
+          width: '90vw',
+          border: '2.4px solid',
+          borderColor: '#ECF39E',
+          position: 'absolute',
+          bottom: '17vh',
+        }} />
+      <div className="contact">
+        צרו קשר:
+        <br />
+        052-5381648
+        <br />
+        israel@gmail.com
+      </div>
     </>
   )
 }
