3a8658345a9388ea7cfff6d22debdbe59a21f7eb
Efrat <118221463+EfratLopez@users.noreply.github.com>
2024-05-23 03:07:42 +0300

    main():shira-changes

--- src/pages/homePage/styles.tsx
+++ src/pages/homePage/styles.tsx
@@ -35,7 +35,7 @@ export const RightRotatedText = styled('div')({
     color: '#a4ac86',
     transform: 'translate(-50%, -50%) rotate(90deg)',
     position: 'absolute',
-    right: '-15%',
+    right: '-22%',
     top: '50%',
     fontSize: 'xx-large',
 })
